8a01bc3da9c6bf1f625f59ffb5c263d8dc6af6b2
Jumvi <judahmvi@gmail.com>
2024-04-29 18:17:29 +0200

    feat:create form for new projet

--- src/pages/creationCompte.jsx
+++ src/pages/creationCompte.jsx
@@ -28,68 +28,67 @@ export default function CreationCompte() {
                 {errors.nom && <p> name is required.</p>}
               </label>
               
-            <label className='font-normale p-2 text-xl  mb-5 rounded w-96 self-center bg-white' >
-              PostNom <span className='text-red-400'>*</span> : 
-                <input {...register('lastName',{required:true})} placeholder='Entrez votre postNom' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
-                {errors.lastName && <p> lastName is required.</p>}
+              <label className='font-normale p-2 text-xl  mb-5 rounded w-96 self-center bg-white' >
+                  PostNom <span className='text-red-400'>*</span> : 
+                  <input {...register('lastName',{required:true})} placeholder='Entrez votre postNom' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.lastName && <p> lastName is required.</p>}
               </label>
-            <label className='font-normale text-xl p-2  mb-5 rounded w-96 self-center bg-white' >
-              Mot de passe <span className='text-red-400'>*</span>: 
-              <input
-                {...register('password', {
-                  required: true,
-                  pattern: {
-                    value: /^(?=.*[A-Z])(?=.*\d).{8,}$/,
-                    message: 'Le mot de passe doit commencer par une majuscule, contenir au moins un chiffre et avoir une longueur minimale de 8 caractères.'
-                 }
-               })}
-               placeholder='mot de passe'
-               className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2'
-              />
-              {errors.password && <p> password is required.</p>}
-            </label>
-            
-            <label className='font-normale text-xl p-2  mb-5 rounded w-96 self-center bg-white' >
-              Email <span className='text-red-400'>*</span>:
-              <input
-                {...register('email', {
-                  required: true,
-                  pattern: {
-                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                    message: 'Adresse e-mail invalide'
+              <label className='font-normale text-xl p-2  mb-5 rounded w-96 self-center bg-white' >
+                Mot de passe <span className='text-red-400'>*</span>: 
+                <input
+                  {...register('password', {
+                    required: true,
+                    pattern: {
+                      value: /^(?=.*[A-Z])(?=.*\d).{8,}$/,
+                      message: 'Le mot de passe doit commencer par une majuscule, contenir au moins un chiffre et avoir une longueur minimale de 8 caractères.'
                   }
                 })}
-                placeholder='Email'
+                placeholder='mot de passe'
                 className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2'
               />
-              {errors.email && <p> email is required.</p>}
-            </label>
+                {errors.password && <p> password is required.</p>}
+              </label>
             
-            <label className='font-normale text-xl  mb-5 p-2 rounded w-96 self-center bg-white' >
-              Téléphone <span className='text-red-400'>*</span> : 
-              <input
-                {...register('phoneNumber', {
-                   required: true,
+              <label className='font-normale text-xl p-2  mb-5 rounded w-96 self-center bg-white' >
+                Email <span className='text-red-400'>*</span>:
+                <input
+                  {...register('email', {
+                    required: true,
                     pattern: {
-                     value: /^\+243\s?[1-9](?:[\s.-]*\d{2}){4}$/,
-                      message: 'Numéro de téléphone invalide'
+                      value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                      message: 'Adresse e-mail invalide'
                     }
-                })}
-                placeholder='Numéro téléphone'
-                className='text-sm font-normal focus:outline-none  rounded-md p-2'
-              />
-              {errors.phoneNumber && <p> phoneNumber is required.</p>}
-            </label>
-           <label className='font-normale text-xl p-2 mb-5 rounded w-96 self-center bg-white' htmlFor="">
-            Type <span className='text-red-400'>*</span> :
-           <label className='text-sm p-2 font-normal'>
-              <input type="checkbox" {...register('entrepreneur')} className='text-xl p-2 font-normal' />
-              Entrepreneur
-            </label>
-            <label className='text-sm p-2 font-normal '>
-              <input type="checkbox" {...register('contributeur')} className='text-xl font-normal'/>
-              contributeur
-            </label>
+                  })}
+                  placeholder='Email'
+                  className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2'
+                />
+                {errors.email && <p> email is required.</p>}
+              </label>
+              <label className='font-normale text-xl  mb-5 p-2 rounded w-96 self-center bg-white' >
+                Téléphone <span className='text-red-400'>*</span> : 
+                <input
+                  {...register('phoneNumber', {
+                    required: true,
+                      pattern: {
+                      value: /^\+243\s?[1-9](?:[\s.-]*\d{2}){4}$/,
+                      message: 'Numéro de téléphone invalide'
+                      }
+                  })}
+                  placeholder='Numéro téléphone'
+                  className='text-sm font-normal focus:outline-none  rounded-md p-2'
+                />
+                {errors.phoneNumber && <p> phoneNumber is required.</p>}
+              </label>
+              <label className='font-normale text-xl p-2 mb-5 rounded w-96 self-center bg-white' htmlFor="">
+                Type <span className='text-red-400'>*</span> :
+              <label className='text-sm p-2 font-normal'>
+                <input type="checkbox" {...register('entrepreneur')} className='text-xl p-2 font-normal' />
+                Entrepreneur
+              </label>
+              <label className='text-sm p-2 font-normal '>
+                <input type="checkbox" {...register('contributeur')} className='text-xl font-normal'/>
+                contributeur
+              </label>
            </label>
             <input type="submit" placeholder='Envoyer' className='bg-lime-400 text-white w-96 h-10 rounded  cursor-pointer self-center mt-10'   />
           </form>
--- src/pages/creationProjet.jsx
+++ src/pages/creationProjet.jsx
@@ -1,7 +1,87 @@
-import React from 'react'
+import React , {useState}from 'react';
+import pik from '../assets/projet.jpg';
+import { useForm } from 'react-hook-form';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+import { GoogleMap,LoadScript } from '@react-google-maps/api';
 
 export default function CreationProjet() {
+  const [address, setAddress] = useState('');
+
+   const handleChange = address => {
+    setAddress(address);
+  };
+ 
+  const    { 
+    register , 
+    handleSubmit , 
+    formState : { errors } , 
+  }  = useForm();
+
   return (
-    <div>C</div>
+    <div className='flex flex-col p-10'>
+      <div className='flex self-center items-center'>
+        <div className='w-1/2'>
+          <img src={pik} alt="projet" className='w-[50rem]' />
+        </div>
+        <div className='w-1/2 bg-gray-100 p-4'>
+          <form className='flex flex-col shadow-lg p-5 ' >
+              <label className='font-normale mb-5 text-xl p-2   w-[33rem] self-center bg-white '>
+                  Titre <span className='text-red-400'>*</span> : 
+                  <input {...register('titre',{required:true})} placeholder='Entrez votre nom' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.titre && <p> name is required.</p>}
+              </label>
+              <label className='font-normale mb-5 text-xl w-[33rem]   self-center '>
+                  Catégorie :
+                  <label className='text-sm p-2 font-normal '>
+                    <input type="checkbox" {...register('agricole')} className='text-xl font-normal'/>
+                    Agricole
+                  </label>
+                  <label className='text-sm p-2 font-normal '>
+                    <input type="checkbox" {...register('agroalimentaire')} className='text-xl font-normal'/>
+                    Agroalimentaire
+                  </label>
+               </label>
+              <label className='font-normale mb-5 text-xl p-2  rounded w-[33rem] self-center bg-white '>
+                  Durée recolte <span className='text-red-400'>*</span> : 
+                  <input {...register('duree',{required:true})} placeholder='Entrez votre nom' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.duree && <p> name is required.</p>}
+              </label>
+              <label className='font-normale mb-5 text-xl p-2  rounded w-[33rem] self-center bg-white '>
+                 localisation <span className='text-red-400'>*</span> : 
+                 <LoadScript
+                 >
+                 </LoadScript>  
+              </label>
+              <label className='font-normale mb-5 text-xl w-[33rem]  self-center  '>
+                  Image projet <span className='text-red-400'>*</span> : 
+                  <input type='file' {...register('imageProjet',{required:true})} placeholder='choisir une image' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.imageProjet && <p> image is required.</p>}
+              </label>
+              <label className='font-normale mb-5 text-xl p-2  rounded w-[33rem] self-center bg-white '>
+                 Recompense contributeur <span className='text-red-400'>*</span> : 
+                  <input {...register('recompense',{required:true})} placeholder='Entrez la déscription de la récompense' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.recompense && <p> recompense is required.</p>}
+              </label>
+              <label className='font-normale mb-5 text-xl p-2 w-[33rem]  self-center  '>
+                Risque et defis du projet <span className='text-red-400'>*</span> : 
+                  <input type='file' {...register('risque',{required:true})} placeholder='Entrez les risques et defis' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.risque && <p> risque is required.</p>}
+              </label>
+              <label className='font-normale mb-5 text-xl p-2  w-[33rem] self-center  '>
+                Auteur du projet <span className='text-red-400'>*</span> : 
+                  <input type='file' {...register('auteur',{required:true})} placeholder='Entrez fichier auteur' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.auteur && <p> auteur is required.</p>}
+              </label>
+              <label className='font-normale mb-5 text-xl p-2  w-[33rem] self-center '>
+                Projet en pdf <span className='text-red-400'>*</span> : 
+                  <input type='file' {...register('projet',{required:true})} placeholder='Entrez fichier projet' className='text-sm p-2 font-normal focus:outline-none  rounded-md p-2' />
+                  {errors.projet && <p> projet is required.</p>}
+              </label>
+              <input type="submit" placeholder='Envoyer' className='bg-lime-400 text-white w-[33rem] h-10 rounded  cursor-pointer self-center mt-10'   />
+              
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
