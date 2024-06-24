e36ea7392a37111e7ea69dc19c633e7c8ae7c38f
Jumvi <judahmvi@gmail.com>
2024-06-24 15:06:55 +0100

    feat:update style for scroll where length over

--- src/components/projectList.jsx
+++ src/components/projectList.jsx
@@ -7,49 +7,51 @@ import { addManagingUser } from '../../config/slicer';
 
 const ProjectList = () => {
   const [projects, setProjects] = useState([]);
-  const navigation = useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const apiUrl = 'http://localhost:3000/projects';
 
-  async function fetchUser(){
-        try {
-            const response = await axios.get(apiUrl);
-            setProjects(response.data);
-            localStorage.setItem('projet',JSON.stringify(response.data));
-        } catch (error) {
-            console.log('Erreur lors de la récupération des utilisateurs', error);
-        }
+  async function fetchProjects(){
+    try {
+      const response = await axios.get(apiUrl);
+      setProjects(response.data);
+      localStorage.setItem('projet', JSON.stringify(response.data));
+    } catch (error) {
+      console.log('Erreur lors de la récupération des projets', error);
+    }
   }
 
-  useEffect(()=>{
-    fetchUser();
-  })
+  useEffect(() => {
+    fetchProjects();
+  }, []);
 
-  const hundleClick = (id)=>{
-        dispatch(addManagingUser('projet',projects));
-        navigation(`/details-project/${id}`);
+  const handleClick = (id) => {
+    dispatch(addManagingUser('projet', projects));
+    navigate(`/details-project/${id}`);
   }
 
   return (
-    <div className="mb-6">
-      <ul className="bg-white shadow-md rounded-lg overflow-hidden">
-        {projects.map(p => (
-          <li key={p.id} className="border-b border-gray-200 p-4">
-            <div className="flex justify-between items-center">
-              <div>
-                <Link onClick={()=>hundleClick(p.id)}>
+    <div className="mb-6 p-5 m-5 border border-green-600">
+      <div className="bg-white shadow-md rounded-lg overflow-hidden max-h-[50vh] overflow-y-auto">
+        <ul>
+          {projects.map(p => (
+            <li key={p.id} className="border-b border-gray-200 p-4">
+              <div className="flex justify-between items-center">
+                <div>
+                  <Link onClick={() => handleClick(p.id)}>
                     <h4 className="font-bold">{p.titre}</h4>
-                </Link>
-                <p>{p.categorie}</p>
+                  </Link>
+                  <p>{p.categorie}</p>
+                </div>
+                <button className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700">
+                  Modifier
+                </button>
               </div>
-              <button  className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700">
-                Modifier
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -34,9 +34,9 @@ const UserList = () => {
   }
 
   return (
-    <div className="mb-6">
+    <div className="mb-6 max-h-[50vh] overflow-y-auto">
       <h3 className="text-xl font-bold mb-4 flex flex-col items-center "></h3>
-      <ul className="bg-white shadow-md rounded-lg overflow-hidden">
+      <ul className="bg-white shadow-md rounded-lg overflow-hidden ">
         {users.map(user => (
           <li key={user.id} className="border-b border-gray-200 p-4">
             <div className="flex justify-between items-center">
