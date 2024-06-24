5dfba10eab422691843e77557aaf48834e4c94fa
Jumvi <judahmvi@gmail.com>
2024-06-24 14:57:55 +0100

    feat:add scroll

--- src/components/contributionListe.jsx
+++ src/components/contributionListe.jsx
@@ -6,54 +6,55 @@ import { Link, useNavigate } from 'react-router-dom';
 import { addContribution } from '../../config/slicer';
 
 const ContributionList = () => {
-  const [contribution, setContribution] = useState([]);
+  const [contributions, setContributions] = useState([]);
   const navigation = useNavigate();
   const dispatch = useDispatch();
 
   const apiUrl = 'http://localhost:3000/contribution';
 
-  async function fetchUser(){
+  async function fetchContributions(){
         try {
             const response = await axios.get(apiUrl);
-            setContribution(response.data.contribution);
-            localStorage.setItem('contribution',JSON.stringify(response.data.contributionn));
+            setContributions(response.data.contribution);
+            localStorage.setItem('contribution', JSON.stringify(response.data.contribution));
         } catch (error) {
-            console.log('Erreur lors de la récupération des utilisateurs', error);
+            console.log('Erreur lors de la récupération des contributions', error);
         }
   }
 
-  useEffect(()=>{
-    fetchUser();
-  })
+  useEffect(() => {
+    fetchContributions();
+  }, []);
 
-  const hundleClick = (id)=>{
-        localStorage.setItem('contribution',JSON.stringify(contribution))
-        dispatch(addContribution(contribution));
+  const handleClick = (id) => {
+        localStorage.setItem('contribution', JSON.stringify(contributions))
+        dispatch(addContribution(contributions));
         navigation(`/contribution/${id}`);
   }
 
   return (
-    <div className="mb-6 border boder-green p-5 m-5">
-      <h3 className="text-2xl font-bold mb-4 flex flex-col items-center text-green-600  uppercase">Gestion des contributions</h3>
-      <ul className="bg-white shadow-md rounded-lg overflow-hidden">
-        {contribution.map(c => (
-          <li key={contribution.id} className="border-b border-gray-200 p-4">
-            <div className="flex justify-between items-center">
-              <div>
-              <p>Titre projet</p>
-
-                <Link onClick={()=>hundleClick(c.id)}>
-                    <h4 className="font-bold">montant:{c.montant}</h4>
-                </Link>
-                <p>Echéance:{c.echeancePaiement}</p>
+    <div className="mb-6 border border-green p-5 m-5">
+      <h3 className="text-2xl font-bold mb-4 flex flex-col items-center text-green-600 uppercase">Gestion des contributions</h3>
+      <div className="bg-white shadow-md rounded-lg overflow-hidden max-h-[50vh] overflow-y-auto">
+        <ul>
+          {contributions.map(c => (
+            <li key={c.id} className="border-b border-gray-200 p-4">
+              <div className="flex justify-between items-center">
+                <div>
+                  <p>Titre projet</p>
+                  <Link onClick={() => handleClick(c.id)}>
+                      <h4 className="font-bold">Montant: {c.montant}</h4>
+                  </Link>
+                  <p>Echéance: {c.echeancePaiement}</p>
+                </div>
+                <button className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700">
+                  Modifier
+                </button>
               </div>
-              <button  className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700">
-                Modifier
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
