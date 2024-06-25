4a9f0eb6b955dc2e89aaa8d250caecbe627e901c
Jumvi <judahmvi@gmail.com>
2024-06-25 14:04:41 +0100

    refact:update render's logical

--- src/components/statistic.jsx
+++ src/components/statistic.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Doughnut, Bar } from 'react-chartjs-2';
-import { Chart } from 'chart.js/auto';
 import { useSelector } from 'react-redux';
 
 
 
-const dataBar = {
-  labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai'],
-  datasets: [
-    {
-      label: 'Investissements',
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(75,192,192,0.6)',
-      hoverBorderColor: 'rgba(75,192,192,1)',
-      data: [65, 59, 80, 81, 56],
-    },
-  ],
-};
+
 
 const Statistics = () => {
   
   const projets = useSelector((state)=>state.auth.projet);
-  console.log(projets);
 
   const doughnutRef = useRef(null);
   const barRef = useRef(null);
 
+  const dataBar = {
+    labels: projets.map(p =>p.titre),
+    datasets: [
+      {
+        label: 'Total reçu',
+        backgroundColor: '#36A2EB',
+        borderColor: 'rgba(75,192,192,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(75,192,192,0.6)',
+        hoverBorderColor: 'rgba(75,192,192,1)',
+        data: projets.map(p=>p.totalRecu),
+      },
+    ],
+  };
+
   const dataDoughnut = {
-    labels: projets.map((project) => project.titre),
+    labels: projets.map((p) => p.titre),
     datasets: [
       {
         data: projets.map((project) => project.budget),
@@ -41,7 +41,7 @@ const Statistics = () => {
 
   useEffect(() => {
     return () => {
-      // Cleanup logic here
+      // nettoyages des graphique afin d'eviter des pertes des ressources et d'éviter des fuites de mémoires lorsque le composant est démonté
       if (doughnutRef.current) {
         doughnutRef.current.destroy();
       }
@@ -52,14 +52,11 @@ const Statistics = () => {
   }, []);
 
   return (
-    <div className=' flex flex-col' >
-     <h2 className="text-2xl font-bold mb-6 flex flex-col items-center m-5 text-green-600">Statistiques</h2>
-
-
-     <div className=' flex bg-green-100 justify-center items-center '>
-
-      <div className="mb-6 ">
-        <h3 className="text-xl mb-4">Répartition des projets</h3>
+    <div className=' flex flex-col bg-green-100' >
+      <h2 className="text-2xl font-bold mb-6 flex flex-col items-center m-5 text-green-600">Statistiques</h2>
+     <div className=' flex  justify-center gap-10 '>
+      <div className="mb-10 p-10">
+        <h3 className="text-xl mb-4 text-green-600">Répartition des projets par budgets</h3>
         <Doughnut
           data={dataDoughnut}
           ref={(chart) => {
@@ -69,13 +66,13 @@ const Statistics = () => {
           }}
         />
       </div> 
-      <div>
-        <h3 className="text-xl mb-4">Investissements mensuels</h3>
+      <div className='mb-10 p-10 '>
+        <h3 className="text-xl mb-4 text-green-600">Répartition projets par investissements</h3>
         <Bar
           data={dataBar}
           ref={(chart) => {
             if (chart) {
-              barRef.current = chart.chartInstance;
+              barRef.current = chart.chartInstance; //stokages des instances du graphique dans la reférence courante
             }
           }}
         />
