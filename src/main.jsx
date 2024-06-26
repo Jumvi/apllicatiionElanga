950863b908c6f0af80ea48f2b4aa6905f9c5fecc
Jumvi <judahmvi@gmail.com>
2024-06-26 16:32:19 +0100

    refact: update config router for add protectedRoutes and adminRoutes components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from '../config/store.js';
 import Home from './pages/home.jsx';
 import Connexion from './pages/connexion.jsx';
 import CreationProjet from './pages/creationProjet.jsx';
@@ -9,15 +11,12 @@ import Contribuer from './pages/contribuer.jsx';
 import Contact from './pages/contact.jsx';
 import CreationCompte from './pages/creationCompte.jsx';
 import OtpVerificationPage from './pages/verificationOtp.jsx';
-import { Provider } from 'react-redux';
-import { store } from '../config/store.js';
 import ForgotPassword from './pages/forgotPassword.jsx';
 import ResetPassword from './pages/resetPassword.jsx';
 import Profil from './pages/profil.jsx';
 import Projects from './pages/projects.jsx';
 import Agricole from './pages/agricole.jsx';
 import Agroalimentaire from './pages/agroalimentaire.jsx';
-import { Path } from 'leaflet';
 import ProjectDetails from './pages/projectDetails.jsx';
 import Dashboard from './pages/dashboard.jsx';
 import ShowProfilUser from './pages/showProfilUser.jsx';
@@ -26,98 +25,96 @@ import UserManagement from './components/userManagement.jsx';
 import ProjectManagement from './components/projectManagement.jsx';
 import Statistics from './components/statistic.jsx';
 import ContributionDetails from './pages/contributionDetails.jsx';
+import { AdminRoute, ProtectedRoute } from './pages/protectedRoutes.jsx';
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<Home/>
+    path: '/',
+    element: <Home />
   },
   {
-    path:'/profil',
-    element:<Profil/>
+    path: '/profil',
+    element: <ProtectedRoute element={<Profil />} />  //on utilise le composant de vérification pour donner accès
   },
   {
-    path:'/connexion',
-    element:<Connexion/>
+    path: '/connexion',
+    element: <Connexion />
   },
   {
-    path:'/creationCompte',
-    element:<CreationCompte/>
+    path: '/creationCompte',
+    element: <CreationCompte />
   },
   {
-    path:'creationProjets',
-    element:<CreationProjet/>
+    path: '/creationProjets',
+    element: <ProtectedRoute element={<CreationProjet />} />
   },
   {
-    path:'/contribuer',
-    element:<Contribuer/>
-  },{
-    
-      path:'/contact',
-      element:<Contact/>
-    
-  },{
-    path:'/otp',
-    element:<OtpVerificationPage/>
-  },{
-    path:'/forgot-password',
-    element:<ForgotPassword/>
-  },{
-    path:'/reset-password',
-    element:<ResetPassword/>
-  },{
-    path:'/projects',
-    element:<Projects/>
-  },{
-    path:'/agricole',
-    element:<Agricole/>
-  },{
-    path:'/agroalimentaire',
-    element:<Agroalimentaire/>
-  },{
-    path:'details-project',
-    children:[
-      {
-        path:':id',
-        element:<ProjectDetails/>
-      }
-    ]
-  },{
-    path:'/dashbord',
-    element:<Dashboard/>
-  },{
-    path:'show-details-users',
-    children:[
-      {
-        path:':id',
-        element:<ShowProfilUser/>
-      }
-    ]
-  },{
-    path:'/users',
-    element:<UserManagement/>
-  },{
-    path:'/projets-list',
-    element:<ProjectManagement/>
-  },{
-    path:'/statistics',
-    element:<Statistics/>
-  },{
-    path:'contribution',
-    element:<ContributionDetails/>,
-    children:[
-      {
-        path:':id',
-        element:<ContributionDetails/>
-      }
-    ]
+    path: '/contribuer',
+    element: <ProtectedRoute element={<Contribuer />} />
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+  },
+  {
+    path: '/otp',
+    element: <OtpVerificationPage />
+  },
+  {
+    path: '/forgot-password',
+    element: <ForgotPassword />
+  },
+  {
+    path: '/reset-password',
+    element: <ResetPassword />
+  },
+  {
+    path: '/projects',
+    element: <Projects />
+  },
+  {
+    path: '/agricole',
+    element: <Agricole />
+  },
+  {
+    path: '/agroalimentaire',
+    element: <Agroalimentaire />
+  },
+  {
+    path: 'details-project/:id',
+    element: <ProtectedRoute element={<ProjectDetails />} />
+  },
+  {
+    path: '/dashboard',
+    element: <AdminRoute element={<Dashboard />} />
+  },
+  {
+    path: 'show-details-users/:id',
+    element: <ProtectedRoute element={<ShowProfilUser />} />
+  },
+  {
+    path: '/users',
+    element: <AdminRoute element={<UserManagement />} />
+  },
+  {
+    path: '/projets-list',
+    element: <AdminRoute element={<ProjectManagement />} />
+  },
+  {
+    path: '/statistics',
+    element: <AdminRoute element={<Statistics />} />
+  },
+  {
+    path: 'contribution/:id',
+    element: <ProtectedRoute element={<ContributionDetails />} />
   }
+]);
 
-])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Provider store={store}> 
-        <RouterProvider router={router}/>
-      </Provider>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
