cf47113a15e4bce12c2e8df2552f6c42bad172d9
Jumvi <judahmvi@gmail.com>
2024-06-28 16:13:14 +0100

    feat: add a seach items features

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -7,6 +7,8 @@ import { addManagingUser } from '../../config/slicer';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [filterUsers,setFilterUsers] = useState([]);
+  const [searchTerm,setSearchTerm] = useState('');
   const navigation = useNavigate();
   const dispatch = useDispatch();
 
@@ -26,6 +28,13 @@ const Users = () => {
     fetchUser();
   })
 
+  useEffect(()=>{
+    setFilterUsers(
+      users.filter((u) =>
+        u.nom.toLowerCase().includes(searchTerm.toLowerCase()))
+    )
+  },[users,searchTerm])
+
   const hundleClick = (id)=>{
         localStorage.setItem('manageUser',JSON.stringify(users))
         dispatch(addManagingUser(users));
@@ -35,8 +44,15 @@ const Users = () => {
   return (
     <div className="mb-6 bg-green-100 p-10 shadow">
       <h3 className="text-xl font-bold mb-4 flex flex-col items-center text-green-600 uppercase">Liste des utilisateurs</h3>
+      <input
+          type="text"
+          placeholder="Rechercher un projet"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-2 border border-green-600 rounded-md mb-2"
+        />
       <ul className="bg-white shadow-md rounded-lg overflow-hidden">
-        {users.map(user => (
+        {filterUsers.map(user => (
           <li key={user.id} className="border-b border-gray-200 p-4">
             <div className="flex justify-between items-center">
               <div>
@@ -44,6 +60,7 @@ const Users = () => {
                     <h4 className="font-bold">{user.nom}</h4>
                 </Link>
                 <p>{user.email}</p>
+                <p>{user.role}</p>
               </div>
               <button  className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700">
                 Modifier
